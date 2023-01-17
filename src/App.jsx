410f1ad0ef37d75f724522f63a76392596d74f21
Agustin-Graciano <agusnachogracianox@gmail.com>
2023-01-17 19:26:45 +0100

    Prototype UI for B2B page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <><>
+      <NavBar />
+    </><div class='flex justify-center'>
+        <div class='w-3/5'>
+          <div class='mt-10 text-xl text-center'>Select wanted delivery date:
+          <label class='inline-block'>
+             <input type="date" name="date" class="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="select date..." />
+          </label>
+          </div>
+
+          <div>
+          <label class='inline-block ml-20 mt-10'>
+             <input type="string" name="Component" class="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter component name..." />
+          </label>
+          <label class='inline-block ml-20 mt-10'>
+             <input type="int" name="Amount" class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1" placeholder="Amount" />
+          </label>
+          <label class='inline-block ml-20 mt-10'>
+             <input type="int" name="Price" class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1" placeholder="Price" />
+          </label>
+          <div class='inline-block text-lg ml-4'>kr ex. VAT</div>
+          </div>
+
+          <div>
+          <label class='inline-block ml-20 mt-6'>
+             <input type="string" name="Component" class="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1" placeholder="Enter component name..." />
+          </label>
+          <label class='inline-block ml-20 mt-6'>
+             <input type="int" name="Amount" class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1" placeholder="Amount" />
+          </label>
+          <label class='inline-block ml-20 mt-6'>
+             <input type="int" name="Price" class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1" placeholder="Price" />
+          </label>
+          <div class='inline-block text-lg ml-4'>kr ex. VAT</div>
+          </div>
+
+          <div>
+          <label class='inline-block ml-20 mt-6'>
+             <input type="string" name="Component" class="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 w-full focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1" placeholder="Enter component name..." />
+          </label>
+          <label class='inline-block ml-20 mt-6'>
+             <input type="int" name="Amount" class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1" placeholder="Amount" />
+          </label>
+          <label class='inline-block ml-20 mt-6'>
+             <input type="int" name="Price" class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1" placeholder="Price" />
+          </label>
+          <div class='inline-block text-lg ml-4'>kr ex. VAT</div> 
+          <div></div>
+          </div>               
+
+          <label class='block ml-20 mt-12'>
+             <input type="string" name="name" class="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1" placeholder="Name" /> 
+          </label>
+
+          <label class='block ml-20 mt-4'>
+             <input type="string" name="E-mail" class="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1" placeholder="E-mail" />
+          </label>
+
+          <label class='block ml-20 mt-4 mb-0'>
+             <input type="int" name="number" class="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1" placeholder="Phone number" />
+          </label>
+          
+          <div class='flex justify-center mt-5 mb-5'>
+          <button class="py-2 px-8 bg-eggplant text-white hover:bg-eg-eggplant rounded drop-shadow-lg">Send Quote</button></div>
+         </div>
+       </div></>
+    )
 }
 
 export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const NavBar = () => {
+
+    return (
+        <div class='bg-eggplant px-2 py-4'> 
+         <div class='flex'>
+         <div><img src="https://cdn.shopify.com/s/files/1/0444/6932/1894/files/Rasmines_eBits_Logo_small_f5522fd4-6199-4817-8b3c-fe2b15dab369_130x@2x.png?v=1661704331" class='flex-none object-contain h-20 w-25 pl-10'></img></div>
+         <div class='absolute inset-8 text-center text-white text-3xl font-bold'>Ebits B2B page prototype</div>
+        </div>
+        </div>
+     )
+  }
+
+  export default NavBar
