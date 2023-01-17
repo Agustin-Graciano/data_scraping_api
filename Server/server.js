9b4728ade61947676f3874beb729a53332e12e9f
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2023-01-17 21:27:08 +0000

    Updating the structure of the files of the application
    I created two new folders "server" and "client". I moved all files from the client side (front-end/React.js) into folder "client" and moved all files from back-end side (server side/node.js) into folder "server"

---
+++ Server/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const puppeteer = require("puppeteer");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let scrapedData;
+const scrapeFunction = async (url) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const [productHeading] = await page.$(
+      '//*[@id="shopify-section-product-template"]/section/div/div[2]/div/div[2]/div/div[2]/div/h1'
+    );
+    const innerTxt = await productHeading.getProperty("textContent");
+    const headingTxt = await innerTxt.jsonValue();
+
+    scrapedData = headingTxt;
+    browser.close();
+  } catch (e) {
+    console.error("The scraping was depricated beacuse of", e);
+  }
+};
+console.log(scrapedData);
+
+scrapeFunction(
+  "https://ebits.dk/collections/mikrokontrollere/products/arduino-nano-r3"
+);
+
+app.get("/api", (req, res) => {
+  res.json({ name: "Bill", age: "99", data: scrapedData });
+});
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
