dbab67515bb98c2f34190a70e47038f5b24f6c72
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2023-01-17 20:11:23 +0000

    First update
    I fixed the "class" instead of "className" error in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,11 +24,11 @@ function App() {
         <div class="w-3/5">
           <div class="mt-10 text-xl text-center">
             Select wanted delivery date:
-            <label class="inline-block">
+            <label className="inline-block">
               <input
                 type="date"
                 name="date"
-                class="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1"
+                className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1"
                 placeholder="select date..."
               />
             </label>
@@ -39,7 +39,7 @@ function App() {
               <input
                 type="string"
                 name="Component"
-                class="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1"
+                className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1"
                 placeholder="Enter component name..."
               />
             </label>
@@ -47,63 +47,63 @@ function App() {
               <input
                 type="int"
                 name="Amount"
-                class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Amount"
               />
             </label>
-            <label class="inline-block ml-20 mt-10">
+            <label className="inline-block ml-20 mt-10">
               <input
                 type="int"
                 name="Price"
-                class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Price"
               />
             </label>
-            <div class="inline-block text-lg ml-4">kr ex. VAT</div>
+            <div className="inline-block text-lg ml-4">kr ex. VAT</div>
           </div>
 
           <div>
-            <label class="inline-block ml-20 mt-6">
+            <label className="inline-block ml-20 mt-6">
               <input
                 type="string"
                 name="Component"
-                class="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1"
+                className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-full rounded-md sm:text-sm focus:ring-1"
                 placeholder="Enter component name..."
               />
             </label>
-            <label class="inline-block ml-20 mt-6">
+            <label className="inline-block ml-20 mt-6">
               <input
                 type="int"
                 name="Amount"
-                class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Amount"
               />
             </label>
-            <label class="inline-block ml-20 mt-6">
+            <label className="inline-block ml-20 mt-6">
               <input
                 type="int"
                 name="Price"
-                class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Price"
               />
             </label>
-            <div class="inline-block text-lg ml-4">kr ex. VAT</div>
+            <div className="inline-block text-lg ml-4">kr ex. VAT</div>
           </div>
 
           <div>
-            <label class="inline-block ml-20 mt-6">
+            <label className="inline-block ml-20 mt-6">
               <input
                 type="string"
                 name="Component"
-                class="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 w-full focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 w-full focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Enter component name..."
               />
             </label>
-            <label class="inline-block ml-20 mt-6">
+            <label className="inline-block ml-20 mt-6">
               <input
                 type="int"
                 name="Amount"
-                class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Amount"
               />
             </label>
@@ -111,11 +111,11 @@ function App() {
               <input
                 type="int"
                 name="Price"
-                class="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Price"
               />
             </label>
-            <div class="inline-block text-lg ml-4">kr ex. VAT</div>
+            <div className="inline-block text-lg ml-4">kr ex. VAT</div>
             <div></div>
           </div>
 
@@ -123,7 +123,7 @@ function App() {
             <input
               type="string"
               name="name"
-              class="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+              className="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
               placeholder="Name"
             />
           </label>
@@ -132,7 +132,7 @@ function App() {
             <input
               type="string"
               name="E-mail"
-              class="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+              className="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
               placeholder="E-mail"
             />
           </label>
@@ -141,13 +141,13 @@ function App() {
             <input
               type="int"
               name="number"
-              class="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+              className="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
               placeholder="Phone number"
             />
           </label>
 
           <div class="flex justify-center mt-5 mb-5">
-            <button class="py-2 px-8 bg-eggplant text-white hover:bg-eg-eggplant rounded drop-shadow-lg">
+            <button className="py-2 px-8 bg-eggplant text-white hover:bg-eg-eggplant rounded drop-shadow-lg">
               Send Quote
             </button>
           </div>
@@ -155,7 +155,6 @@ function App() {
       </div>
     </>
   );
-  return <div className="App"></div>;
 }
 
 export default App;
