407e33924e79974627bccd76790fe40d6fc480a3
Fectician <nicolas.lauridsen@gmail.com>
2023-01-18 15:44:39 +0100

    Changed: Client fetch now points to the localhost port used for running the server, and the server now has CORS configured (later, more configuration required), and correctly points to a designated element to fetch from the ebits website.

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const puppeteer = require("puppeteer");
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,17 +18,25 @@ const scrapeFunction = async (url) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    const productHeading = await page.waitForSelector(".product-meta__title");
-    // const [productHeading] = await page.$(
-    //   '//*[@id="shopify-section-product-template"]/section/div/div[2]/div/div[2]/div/div[2]/div/h1'
-    // );
-    const innerTxt = await productHeading.getProperty("textContent");
-    const headingTxt = await innerTxt.jsonValue();
 
+    const productHeading = await page.waitForSelector('.product-meta__title');
+    /*
+    const [productHeading] = await page.$(
+      '//*[@id="shopify-section-product-template"]/section/div/div[1]/div/div[1]/div/div[1]/div/h1'
+    );
+    */
+
+
+    const innerTxt = await productHeading.getProperty("textContent");
+    
+    const headingTxt = await innerTxt.jsonValue();
     scrapedData = headingTxt;
+
+
+
     browser.close();
   } catch (e) {
-    console.error("The scraping was depricated beacuse of", e);
+    console.error("The scraping was deprecated because of", e);
   }
 };
 console.log(scrapedData);
@@ -33,7 +46,7 @@ scrapeFunction(
 );
 
 app.get("/api", (req, res) => {
-  res.json({ name: "Bill", age: "99", data: scrapedData });
+    res.json({ name: "Bill", age: "99", data: scrapedData });
 });
 
 app.listen(5000, () => {
