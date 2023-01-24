7d45688ab8d4af8fb9fcabfdc484a6878b2d1032
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2023-01-24 21:52:53 +0000

    I tried to make the filtering function user triggered
    I tried to make the filtering function to be called by user interaction. I set a state variable that gets the user input. Then I assigned the filtering function to be hooked for the button. On click, the functions filters the array for elements that fit the criteria set by the user input. As well a state variable was assigned for the output. (It's hard to test the function since i need to wait for the data to load otherwise it returns an empty array.)

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,11 +5,16 @@ import { Button } from "@material-tailwind/react";
 
 function App() {
   const [data, setData] = useState([]);
-  const filterData = (data) => {
+
+  const [userSearch, setUserSearch] = useState("");
+
+  const [output, setOutput] = useState("");
+
+  const filterData = (data, inputCriteria) => {
     const filterResult = data.filter(
-      (object) =>
-        object.title == "Læs kundernes anmeldelser af www.aliexpress.com"
+      (object) => object.title.toLowerCase() === inputCriteria.toLowerCase()
     );
+    setOutput(filterResult);
     console.log(filterResult);
   };
   useEffect(() => {
@@ -20,12 +25,12 @@ function App() {
           setData(data);
           console.log(data[9].title);
         });
-      filterData(data);
     };
 
     fetchData();
   }, []);
 
+  console.log(userSearch);
   // const filterData = (data) => {
   //   const filterResult = data.filter(
   //     (object) => object.title == "2.54mm 1*40P Color Single Row Needle"
@@ -43,23 +48,27 @@ function App() {
           <div className="mt-10 text-xl text-center">
             Select wanted delivery date:
             <span className="">
-            <input
-              type="date"
-              name="date"
-              className="ml-2 mt-1 px-4 py-2 w-1/4 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
-              placeholder="select date..."
-            />
+              <input
+                type="date"
+                name="date"
+                className="ml-2 mt-1 px-4 py-2 w-1/4 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                placeholder="select date..."
+              />
             </span>
           </div>
 
           <div>
             <span className="inline-block ml-20 mt-10">
-            <input
-              type="string"
-              name="Component"
-              className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
-              placeholder="Enter component name..."
-            />
+              <input
+                type="string"
+                name="Component"
+                className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                placeholder="Enter component name..."
+                onChange={(event) => {
+                  setUserSearch(event.target.value);
+                }}
+              />
+              {output !== 0 ? output.title : ""}
             </span>
 
             <span className="inline-block ml-20 mt-10">
@@ -174,14 +183,13 @@ function App() {
 
           <form method="post" action="http://localhost:5000/pyth">
             <div>
-
               <span className="inline-block ml-20 mt-10">
-              <input
-                type="float"
-                name="Price"
-                className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
-                placeholder="Price"
-              />
+                <input
+                  type="float"
+                  name="Price"
+                  className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                  placeholder="Price"
+                />
               </span>
               <div className="inline-block text-lg ml-4"></div>
 
@@ -225,18 +233,22 @@ function App() {
             <div className="mt-10 text-xl text-center">
               Select wanted delivery date:
               <span className="">
-              <input
-                type="date"
-                name="Date"
-                className="ml-2 mt-1 px-4 py-2 w-1/4 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
-                placeholder="select date..."
-              />
+                <input
+                  type="date"
+                  name="Date"
+                  className="ml-2 mt-1 px-4 py-2 w-1/4 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
+                  placeholder="select date..."
+                />
               </span>
             </div>
 
             <div className="flex justify-center mt-5 mb-5">
               <Button
-                type="submit"
+                onClick={
+                  userSearch.length === 0
+                    ? () => setOutput("Please fill out the field")
+                    : () => filterData(data, userSearch)
+                }
                 value="submit"
                 className="m-5 p-2 bg-eggplant text-white rounded drop-shadow-lg"
               >
