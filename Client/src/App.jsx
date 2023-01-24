e58d48115e39e60707d157122f910f831a26e1db
Agustin-Graciano <agusnachogracianox@gmail.com>
2023-01-24 16:15:54 +0100

    Invalid DOM property `class` Fixed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -41,188 +41,203 @@ function App() {
         <div className="w-3/5">
           <div className="mt-10 text-xl text-center">
             Select wanted delivery date:
-            <label className="inline-block" />
+            <span className="">
             <input
               type="date"
               name="date"
               className="ml-2 mt-1 px-4 py-2 w-1/4 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
               placeholder="select date..."
             />
+            </span>
           </div>
 
           <div>
-            <label class="inline-block ml-20 mt-10" />
+            <span className="inline-block ml-20 mt-10">
             <input
               type="string"
               name="Component"
               className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
               placeholder="Enter component name..."
             />
+            </span>
 
-            <label class="inline-block ml-20 mt-10">
+            <span className="inline-block ml-20 mt-10">
               <input
                 type="int"
                 name="Amount"
                 className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Amount"
               />
-            </label>
-            <label className="inline-block ml-20 mt-10">
+            </span>
+
+            <span className="inline-block ml-20 mt-10">
               <input
                 type="int"
                 name="Price"
                 className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Price"
               />
-            </label>
+            </span>
             <div className="inline-block text-lg ml-4">kr ex. VAT</div>
           </div>
 
           <div>
-            <label className="inline-block ml-20 mt-6">
+            <span className="inline-block ml-20 mt-6">
               <input
                 type="string"
                 name="Component"
                 className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Enter component name..."
               />
-            </label>
-            <label className="inline-block ml-20 mt-6">
+            </span>
+
+            <span className="inline-block ml-20 mt-6">
               <input
                 type="int"
                 name="Amount"
                 className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Amount"
               />
-            </label>
-            <label className="inline-block ml-20 mt-6">
+            </span>
+
+            <span className="inline-block ml-20 mt-6">
               <input
                 type="int"
                 name="Price"
                 className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Price"
               />
-            </label>
+            </span>
+
             <div className="inline-block text-lg ml-4">kr ex. VAT</div>
           </div>
 
           <div>
-            <label className="inline-block ml-20 mt-6">
+            <span className="inline-block ml-20 mt-6">
               <input
                 type="string"
                 name="Component"
                 className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Enter component name..."
               />
-            </label>
-            <label className="inline-block ml-20 mt-6">
+            </span>
+
+            <span className="inline-block ml-20 mt-6">
               <input
                 type="int"
                 name="Amount"
                 className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Amount"
               />
-            </label>
-            <label class="inline-block ml-20 mt-6">
+            </span>
+
+            <span className="inline-block ml-20 mt-6">
               <input
                 type="int"
                 name="Price"
                 className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Price"
               />
-            </label>
+            </span>
+
             <div className="inline-block text-lg ml-4">kr ex. VAT</div>
             <div></div>
           </div>
 
-          <label class="block ml-20 mt-12">
+          <span className="block ml-20 mt-12">
             <input
               type="string"
               name="name"
               className="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
               placeholder="Name"
             />
-          </label>
+          </span>
 
-          <label class="block ml-20 mt-4">
+          <span className="block ml-20 mt-4">
             <input
               type="string"
               name="E-mail"
               className="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
               placeholder="E-mail"
             />
-          </label>
+          </span>
 
-          <label class="block ml-20 mt-4 mb-0">
+          <span className="block ml-20 mt-4 mb-0">
             <input
               type="int"
               name="number"
               className="w-1/4 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
               placeholder="Phone number"
             />
-          </label>
+          </span>
 
           <form method="post" action="http://localhost:5000/pyth">
             <div>
-              <label class="inline-block ml-20 mt-10" />
+
+              <span className="inline-block ml-20 mt-10">
               <input
                 type="float"
                 name="Price"
                 className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="Price"
               />
+              </span>
               <div className="inline-block text-lg ml-4"></div>
 
-              <label class="inline-block ml-20 mt-10">
+              <span className="inline-block ml-20 mt-10">
                 <input
                   type="int"
                   name="Amount"
                   className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                   placeholder="Amount"
                 />
-              </label>
-              <label className="inline-block ml-20 mt-10">
+              </span>
+
+              <span className="inline-block ml-20 mt-10">
                 <input
                   type="string"
                   name="Currency"
                   className="w-24 ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                   placeholder="Currency in 3 letters"
                 />
-              </label>
-              <label className="inline-block ml-20 mt-10">
+              </span>
+
+              <span className="inline-block ml-20 mt-10">
                 <input
                   type="string"
                   name="OutsideEbits"
                   className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                   placeholder="OutsideEbits"
                 />
-              </label>
-              <label className="inline-block ml-20 mt-10">
+              </span>
+
+              <span className="inline-block ml-20 mt-10">
                 <input
                   type="string"
                   name="OutsideEU"
                   className="ml-2 mt-1 px-4 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                   placeholder="OutsideEU"
                 />
-              </label>
+              </span>
             </div>
 
             <div className="mt-10 text-xl text-center">
               Select wanted delivery date:
-              <label className="inline-block" />
+              <span className="">
               <input
                 type="date"
                 name="Date"
                 className="ml-2 mt-1 px-4 py-2 w-1/4 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md sm:text-sm focus:ring-1"
                 placeholder="select date..."
               />
+              </span>
             </div>
 
-            <div class="flex justify-center mt-5 mb-5">
+            <div className="flex justify-center mt-5 mb-5">
               <Button
                 type="submit"
                 value="submit"
-                class="m-5 p-2 bg-eggplant text-white rounded drop-shadow-lg"
+                className="m-5 p-2 bg-eggplant text-white rounded drop-shadow-lg"
               >
                 Send Quote
               </Button>
